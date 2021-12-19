3e063dee8b2e3f78a5402ba3d5d3997571198990
Michelle Guan <michelle.guan93@gmail.com>
2021-12-19 20:55:32 +0000

    Implement Flood Fill using breadth first search

--- binarytree.js
+++ binarytree.js
@@ -38,8 +38,60 @@ var numIslands = function(grid) {
 };
 
 
+
+
+
+
 var isValidBST = function(root) {
     
 };
 
 // binary tree = each node has a parent and a left child and a right child
+
+var floodFill = function(image, sr, sc, newColor) {
+    var startColor = image[sr][sc]; 
+    var visited = new Set(); 
+    var queue = [[sr, sc]]; 
+ 
+    if (startColor === newColor) {
+        return image; 
+    }
+    
+    while (queue.length > 0) {
+        var pixel = queue.pop(); 
+        visited.add(pixel); 
+        image[pixel[0]][pixel[1]] = newColor
+  
+        var neighbors = getNeighbors(image, pixel[0], pixel[1], startColor); 
+        for (var i = 0; i < neighbors.length; i++) {
+            var current = neighbors[i];
+            if (!visited.has(current)) {
+                queue.push(current); 
+            }
+        }
+    }
+
+    return image; 
+};
+
+var getNeighbors = function(grid, rowIndex, colIndex, startColor) {
+    var neighbors = []; 
+    // list all possible neighbors
+    var potentialNeighbors = [[rowIndex-1, colIndex], [rowIndex+1, colIndex], [rowIndex, colIndex+1], [rowIndex, colIndex-1]]
+    // must validate whether or not a neighbor is at the edge
+    for (var i = 0; i < potentialNeighbors.length; i++) {
+        var current = potentialNeighbors[i]; 
+        if (isValid(grid,current)) {
+            var color = grid[current[0]][current[1]]; 
+            if (color === startColor) {
+                neighbors.push(current); 
+            }
+        }
+    }
+    // return array of coordinates for its left, right, up, and down neighbors
+    return neighbors; 
+}
+
+var isValid = function (grid, current) {
+    return (current[0] >= 0 && current[1] >= 0 && current[0] < grid.length && current[1] < grid[0].length)
+}
