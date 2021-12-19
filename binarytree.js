20b74c2bbdebbb308281354974ed6e126f9867aa
Michelle Guan <michelle.guan93@gmail.com>
2021-12-19 07:15:41 +0000

    Implement number of islands breadth first search

--- binarytree.js
+++ binarytree.js
@@ -1,3 +1,43 @@
+var numIslands = function(grid) {
+    const direction = [[-1,0],[1,0],[0,1],[0,-1]];
+    let result = 0;
+    
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (grid[i][j] === "1") {
+                result += 1;
+                grid[i][j] = "2";
+                bfs(i, j);
+            }
+        }
+    }
+    
+    
+    function bfs(y, x) {
+        const queue = [[y, x]];
+        
+        while (queue.length > 0) {
+            const curr = queue.shift();
+            const dy = curr[0], dx = curr[1];
+            
+            for (let i = 0; i < 4; i++) {
+                let ny = dy + direction[i][0];
+                let nx = dx + direction[i][1];
+                if (ny < 0 || nx < 0 || ny >= grid.length || nx >= grid[0].length) {
+                    continue;   
+                }
+                if (grid[ny][nx] === "1") {
+                    grid[ny][nx] = "2";
+                    queue.push([ny, nx]);
+                }
+            }
+        }
+    }
+    
+    return result;
+};
+
+
 var isValidBST = function(root) {
     
 };
