a6f23332207ccc0aca0ddd417ebe2f98cc3794cd
Michelle Guan <michelle.guan93@gmail.com>
2022-01-10 23:51:01 +0000

    checkers practice

---
+++ checkers.js
@@ -0,0 +1,57 @@
+class Checkers {
+  constructor (size) {
+    this.board = []; 
+    this.size = size; 
+  }
+
+
+  makeBoard () {
+    for (var i = 0; i < this.size; i++) {
+      var row = []; 
+      if (i % 2 === 0) {
+        var color = 'w'; 
+      } else {
+        var color = 'b'; 
+      }
+      for (var j = 0; j < this.size; j++) {
+        row.push(color);
+        if (color === 'w') {
+          color = 'b'; 
+        } else {
+          color = 'w'; 
+        }
+      }
+      this.board.push(row); 
+    }
+  }
+
+  
+  populateWhite () {
+    for (var i = 0; i < 3; i++) {
+      for (var j = 0; j < this.size; j++) {
+        if (this.board[i][j] === 'b') {
+          this.board[i][j] = 'x'; 
+        }
+      }
+    }
+  }
+
+  populateBlack () { 
+    for (var i = this.size - 1; i > this.size - 4; i--) {
+      console.log('hello'); 
+      for (var j = 0; j < this.size; j++) {
+        if (this.board[i][j] === 'b') {
+          this.board[i][j] = 'y'; 
+        }
+      }
+    }
+  }
+
+  
+}
+
+var checkers = new Checkers(8); 
+checkers.makeBoard(); 
+console.log('populate black', checkers.populateBlack()); 
+console.log('populate white', checkers.populateWhite()); 
+console.log('populated board', checkers.board); 
